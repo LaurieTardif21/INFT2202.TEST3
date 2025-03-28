387246ada77330504a2763c343d96c860212d4cf
laurie tardif <laurieanne.easytardif@dcmail.ca>
2025-03-28 10:33:51 -0400

    modified:   INFT2202.TEST3/src/server/app.js

--- INFT2202.TEST3/src/server/app.js
+++ INFT2202.TEST3/src/server/app.js
@@ -49,11 +49,16 @@ router.get('/api/movies', (req, res) => {
 
 // configure the server to use your new router instance
 app.use(router);
+
 // automatically serve static assets from the client folder AFTER defining the routes.
 app.use(express.static(path.join(__dirname, '../..', 'client')));
 
+// automatically serve static assets from the client folder AFTER defining the routes.
+app.use('/js', express.static(path.join(__dirname, '../..', 'client', 'js')));
+
 // automatically serve static assets from the node_modules folder
-app.use('/node_modules', express.static('node_modules'));
+app.use('/node_modules', express.static(path.join(__dirname, '..', '..', 'node_modules')));
+
 
 // start the server
 app.listen(PORT, () => {
